b2786ef9c83cb34179c79167570c2f4740338909
iamzainn <iamzainnf@gmail.com>
2024-07-18 23:03:07 +0500

    fix prod url

--- src/actions.ts
+++ src/actions.ts
@@ -345,11 +345,11 @@ export async function checkOut() {
       success_url:
         process.env.NODE_ENV === "development"
           ? "http://localhost:3000/payment/success"
-          : "https://shoe-marshal.vercel.app/payment/success",
+          : "https://malik-ecomerce.vercel.app/payment/success",
       cancel_url:
         process.env.NODE_ENV === "development"
           ? "http://localhost:3000/payment/cancel"
-          : "https://shoe-marshal.vercel.app/payment/cancel",
+          : "https://malik-ecomerce.vercel.app/payment/cancel",
       metadata: {
         userId: user.id,
       },
